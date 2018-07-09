442287a8d14468a64006ce28d14f90efccfe3951
Matt Messersmith <messersmith.matt@gmail.com>
2018-07-09 07:31:54 -0400

    Test to verify we produce cpp code for a given card. Need to also run a gtest to make sure it all compiles.

--- src/python/heartspeed/cards/generate_card_classes.py
+++ src/python/heartspeed/cards/generate_card_classes.py
@@ -29,14 +29,61 @@ class JsonCardGenerator:
 #         for card in card_list:
 #             self._generate_class_file(card)
 
-# class CardClassGenerator:
-#     """ Responsible for generate a single C++ class based on the card json """
-#     def __init__(self, card_json):
-#         self.card_json = card_json
-#         self.class_str = self._generate_class_str()
+class CardClassGenerator:
+    """ Responsible for generate a single C++ class based on the card json """
+    def __init__(self, card_json):
+        self.card_json = card_json
 
-#     def generate_class_file(self, filename=None):
-#         if filename is None:
-#             filename = os.path.join(
-#         with open('', 'w') as f:
-#             f.write(self.class_str)
+    def generate_class_file(self, cards_cpp_dir=None):
+        if cards_cpp_dir is None:
+            this_dir = os.path.dirname(os.path.realpath(__file__))
+            cards_cpp_dir = os.path.join(this_dir, "..", "..", "..", "cpp", "cards")
+        name = self._get_name()
+        cpp_file = os.path.join(cards_cpp_dir, name + ".cpp")
+        hpp_file = os.path.join(cards_cpp_dir, name + ".hpp")
+        with open(cpp_file, 'w') as f:
+            cpp_str = self._gen_cpp_string()
+            f.write(cpp_str)
+        with open(hpp_file, 'w') as f:
+            hpp_str = self._gen_hpp_string()
+            f.write(hpp_str)
+
+    def _get_name(self):
+        return self.card_json["name"].replace(" ", "")
+
+    def _gen_hpp_string(self):
+        template = """#include "BaseCard.hpp"
+
+class {0} : public BaseCard {{
+public:
+    {0}();
+    std::string get_name();
+    int get_cost();
+    std::string get_id();
+    std::string get_text();
+private:
+    std::string name;
+    int cost;
+    std::string id;
+    std::string text;
+}};"""
+
+        return template.format(self._get_name())
+
+    def _gen_cpp_string(self):
+        template = """#include "{0}.hpp"
+
+{0}::{0}() : name("{0}"),
+			   cost({1}),
+			   id("{2}"),
+			   text("{3}") {{}}
+
+std::string {0}::get_name() {{ return name; }}
+int {0}::get_cost() {{ return cost; }}
+std::string {0}::get_id() {{ return id; }}
+std::string {0}::get_text() {{ return text; }}"""
+
+        cost = self.card_json["cost"]
+        id = self.card_json["id"]
+        text = self.card_json["text"]
+        return template.format(self._get_name(), cost, id, text)
--- src/python/test/unit/test_generate_card_class.py
+++ src/python/test/unit/test_generate_card_class.py
@@ -1,12 +1,34 @@
-# from heartspeed.cards.generate_card_classes import JsonCardGenerator, CardClassGenerator
+import json
+import os
 
-# def test_generate_card_class():
-#     json_str = """{'artist': 'Nutthapon Petchthai', 'cardClass': 'MAGE', 'collectible': True, 'cost': 5, 'dbfId': 2539, 'flavor': "It's on the rack next to ice lance, acid lance, and English muffin lance.", 'id': 'AT_001', 'name': 'Flame Lance', 'playRequirements': {'REQ_MINION_TARGET': 0, 'REQ_TARGET_TO_PLAY': 0}, 'rarity': 'COMMON', 'set': 'TGT', 'text': 'Deal $8 damage to a minion.', 'type': 'SPELL'}"""
-#     card_json = json.loads(json_str)
-#     gen = CardClassGenerator(card_json)
+from heartspeed.cards.generate_card_classes import JsonCardGenerator, CardClassGenerator
+
+def test_generate_card_class():
+    json_str = """{"artist":"Nutthapon Petchthai","cardClass":"MAGE","collectible":true,"cost":5,"dbfId":2539,"flavor":"It's on the rack next to ice lance, acid lance, and English muffin lance.","id":"AT_001","name":"Flame Lance","playRequirements":{"REQ_MINION_TARGET":0,"REQ_TARGET_TO_PLAY":0},"rarity":"COMMON","set":"TGT","text":"Deal $8 damage to a minion.","type":"SPELL"}"""
+    card_json = json.loads(json_str)
+    gen = CardClassGenerator(card_json)
+
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    gen.generate_class_file(cards_cpp_dir=this_dir)
+
+    expected_cpp_fname = os.path.join(this_dir, "FlameLance.cpp")
+    expected_hpp_fname = os.path.join(this_dir, "FlameLance.hpp")
+
+    assert os.path.exists(expected_cpp_fname)
+    assert os.path.exists(expected_hpp_fname)
+
+    # Do some spot checking...
+    with open(expected_cpp_fname, 'r') as f:
+        text = f.read()
+        assert "FlameLance::FlameLance()" in text
+        assert 'name("FlameLance")' in text
+        assert 'id("AT_001")' in text
+        assert 'text("Deal $8 damage to a minion.")' in text
     
-#     assert ": BaseCard" in gen.class_str
-#     assert 'name("Flame Lance")' in gen.class_str
+    with open(expected_hpp_fname, 'r') as f:
+        text = f.read()
+        assert "class FlameLance : public BaseCard" in text
+        assert "get_name" in text
 
-def test(): # Placeholder until we get a unit test.
-    assert 1
+    os.remove(expected_cpp_fname)
+    os.remove(expected_hpp_fname)

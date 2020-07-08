5263fce6893094681713fb701e620b5b8818b9df
Matt Messersmith <messersmith.matt@gmail.com>
2020-07-08 08:51:37 -0400

    More tests.

--- src/cpp/bg_game/board_test.cpp
+++ src/cpp/bg_game/board_test.cpp
@@ -3,10 +3,25 @@
 #include "../cards/bgs/BgBaseCard.hpp"
 #include "../test/googletest/include/gtest/gtest.h"
 
-TEST(Board, CanCalculateDamage) {
+TEST(Board, CanCalculateDamageEmptyBoard) {
+    auto f = BgCardFactory();
+    std::vector<BgBaseCard> cards;
+    auto board = Board(cards);
+    EXPECT_EQ(board.calculate_damage(), 0);
+}
+
+TEST(Board, CanCalculateDamageOneCard) {
     auto f = BgCardFactory();
     auto goldrinn = f.get_card("Goldrinn, the Great Wolf");
     std::vector<BgBaseCard> cards{ goldrinn };
     auto board = Board(cards);
     EXPECT_EQ(board.calculate_damage(), 5);
 }
+
+// TEST(Board, CanCalculateDamageWithFullBoard) {
+//     auto f = BgCardFactory();
+//     auto goldrinn = f.get_card("Alleycat");
+//     std::vector<BgBaseCard> cards{ goldrinn };
+//     auto board = Board(cards);
+//     EXPECT_EQ(board.calculate_damage(), 5);
+// }

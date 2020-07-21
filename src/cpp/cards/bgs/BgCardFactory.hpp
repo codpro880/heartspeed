b96cdf651cddbc6cefc848d1c5c4d501c71ac71a
Matt Messersmith <messersmith.matt@gmail.com>
2020-07-21 09:28:57 -0400

    Small refactoring to start using maps.

--- src/cpp/cards/bgs/BgCardFactory.hpp
+++ src/cpp/cards/bgs/BgCardFactory.hpp
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 #include "BgBaseCard.hpp"
 
 // This factory is for auto generated cards
 class BgBaseCardFactory {
 public:
+    BgBaseCardFactory() {}
     virtual BgBaseCard get_card(std::string name);
+private:
+    std::unordered_map<std::string, BgBaseCard> cards;
 };
 
 // This factory is for hand generated cards
 class BgCardFactory : public BgBaseCardFactory {
 public:
+    BgCardFactory() { init_cards(); }
     BgBaseCard get_card(std::string name);
-    
+private:
+    void init_cards();
+    std::unordered_map<std::string, BgBaseCard> cards;
 };

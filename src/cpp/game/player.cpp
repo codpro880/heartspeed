341ce045ec88e8d53313d518df6bd654a87f217b
Matt Messersmith <messersmith.matt@gmail.com>
2020-10-30 17:56:34 -0400

    Removing standard/game directories. Not needed for battlegrounds.

--- src/cpp/game/player.cpp
+++
@@ -1,9 +0,0 @@
-#include "player.hpp"
-
-void Player::take_damage(int damage) { // I feel as if this will become complicated
-    health -= damage;
-}
-
-void Player::heal(int damage) {
-    health += damage;
-}

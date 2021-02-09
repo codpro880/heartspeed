f49ef4450099d55b36c3c30198d21ce7c77f45e7
Matt Messersmith <messersmith.matt@gmail.com>
2021-02-08 19:32:11 -0500

    Added targeted battlecry available actions.

--- src/bgs_engine/bg_game/player_test.cpp
+++ src/bgs_engine/bg_game/player_test.cpp
@@ -2299,42 +2299,42 @@ TEST(Player, CanListPlayActionsAlmostFullBoard) {
     }
 }
 
-// TEST(Player, CanListPlayActionsForTargettedBattlecriesFullBoard) {
-//     // Board is almost full, so every slot should be available
-//     auto f = BgCardFactory();
-//     std::vector<std::shared_ptr<BgBaseCard> > b1_cards
-//         {
-//          f.get_card("Murloc Tidehunter"),
-//          f.get_card("Alleycat"),
-//          f.get_card("Vulgar Homunculus"),
-//          f.get_card("Alleycat"),
-//          f.get_card("Alleycat"),
-//          f.get_card("Murloc Tidehunter"),
-//         };
-//     std::shared_ptr<Board> board1(new Board(b1_cards));
+TEST(Player, CanListPlayActionsForTargettedBattlecriesFullBoard) {
+    // Board is almost full, so every slot should be available
+    auto f = BgCardFactory();
+    std::vector<std::shared_ptr<BgBaseCard> > b1_cards
+        {
+         f.get_card("Murloc Tidehunter"),
+         f.get_card("Alleycat"),
+         f.get_card("Vulgar Homunculus"),
+         f.get_card("Alleycat"),
+         f.get_card("Alleycat"),
+         f.get_card("Murloc Tidehunter"),
+        };
+    std::shared_ptr<Board> board1(new Board(b1_cards));
 
-//     std::vector<std::shared_ptr<BgBaseCard> > hand_cards
-//         {
-//          f.get_card("Vermin Sensei"), // Three targets
-//          f.get_card("Rockpool Hunter"), // Two target
-//          f.get_card("Nathrezim Overseer"), // One target
-//          f.get_card("Mal'Ganis (Golden)") // Not targetted
-//         };
-//     auto hand = Hand(hand_cards);
-//     auto player = Player(hand, "Test");
-//     player.set_board(board1);
+    std::vector<std::shared_ptr<BgBaseCard> > hand_cards
+        {
+         f.get_card("Virmen Sensei"), // Three targets
+         f.get_card("Rockpool Hunter"), // Two target
+         f.get_card("Nathrezim Overseer"), // One target
+         f.get_card("Mal'Ganis (Golden)") // Not targetted
+        };
+    auto hand = Hand(hand_cards);
+    auto player = Player(hand, "Test");
+    player.set_board(board1);
 
-//     // Assert we can't play any minions (board is full)
-//     auto all_actions = player.list_all_possible_actions();
-//     auto play_actions = player.list_play_from_hand_actions();
-//     // Vermin can go in any slot, has three targets (3 * 7)
-//     // Rockpool can go in any slot, has two targets (2 * 7)
-//     // Nathrezim can go in any slot, has one target (1 * 7)
-//     // Mal'Ganis can go in any slot, no target (1 * 7)
-//     EXPECT_EQ(play_actions.size(), (unsigned)(3*7 + 2*7 + 1*7 + 7));
-//     for (const auto& play_action : play_actions) {
-//         EXPECT_TRUE(std::find(all_actions.begin(),
-//                               all_actions.end(),
-//                               play_action) != all_actions.end());
-//     }
-// }
+    // Assert we can't play any minions (board is full)
+    auto all_actions = player.list_all_possible_actions();
+    auto play_actions = player.list_play_from_hand_actions();
+    // Vermin can go in any slot, has three targets (3 * 7)
+    // Rockpool can go in any slot, has two targets (2 * 7)
+    // Nathrezim can go in any slot, has one target (1 * 7)
+    // Mal'Ganis can go in any slot, no target (1 * 7)
+    EXPECT_EQ(play_actions.size(), (unsigned)(3*7 + 2*7 + 1*7 + 7));
+    for (const auto& play_action : play_actions) {
+        EXPECT_TRUE(std::find(all_actions.begin(),
+                              all_actions.end(),
+                              play_action) != all_actions.end());
+    }
+}

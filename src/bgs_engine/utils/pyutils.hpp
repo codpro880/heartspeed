ba66d6e768a4fd66ce2f4fbb18aeadefdf5f306a
Matt Messersmith <messersmith.matt@gmail.com>
2021-02-21 10:48:10 -0500

    Starting some utilities...

--- src/bgs_engine/utils/pyutils.hpp
+++ src/bgs_engine/utils/pyutils.hpp
@@ -1,5 +1,6 @@
 namespace pyutils {
-    bool contains(std::vector<std::string> vec, std::string to_find) {
+    template <typename T1, typename T2>
+    bool contains(std::vector<T1> vec, T2 to_find) {
         return std::find(vec.begin(), vec.end(), to_find) != vec.end();
     }
                                                               
--- src/bgs_engine/utils/pyutils_test.cpp
+++ src/bgs_engine/utils/pyutils_test.cpp
@@ -1,6 +1,15 @@
+#include "../test/googletest/include/gtest/gtest.h"
+
 #include "pyutils.hpp"
 
-TEST(PyUtils, CanDoVectorContainment) {
+TEST(PyUtils, CanDoVectorContainmentStr) {
     std::vector<std::string> test({"one", "two", "three", "four"});
-    EXPECT_EQ(pyutils::contains(test, "one"));
+    EXPECT_TRUE(pyutils::contains(test, "one"));
+    EXPECT_FALSE(pyutils::contains(test, "five"));
+}
+
+TEST(PyUtils, CanDoVectorContainmentInt) {
+    std::vector<int> test({1, 2, 3, 4});
+    EXPECT_TRUE(pyutils::contains(test, 3));
+    EXPECT_FALSE(pyutils::contains(test, 8));
 }

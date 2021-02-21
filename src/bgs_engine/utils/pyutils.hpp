4d7c11526d11e699a3415394011de4354a12ede6
Matt Messersmith <messersmith.matt@gmail.com>
2021-02-21 11:08:17 -0500

    Adjusting utilities, also slowly cleaning up baseline where they're used.

--- src/bgs_engine/utils/pyutils.hpp
+++ src/bgs_engine/utils/pyutils.hpp
@@ -1,7 +1,20 @@
+#include <set>
+#include <vector>
+#include <unordered_set>
+
 namespace pyutils {
     template <typename T1, typename T2>
-    bool contains(std::vector<T1> vec, T2 to_find) {
+    bool in(T1 to_find, std::vector<T2> vec) {
         return std::find(vec.begin(), vec.end(), to_find) != vec.end();
     }
-                                                              
+
+    template <typename T1, typename T2>
+    bool in(T1 to_find, std::set<T2> vec) {
+        return std::find(vec.begin(), vec.end(), to_find) != vec.end();
+    }
+
+    template <typename T1, typename T2>
+    bool in(T1 to_find, std::unordered_set<T2> vec) {
+        return std::find(vec.begin(), vec.end(), to_find) != vec.end();
+    }
 }
--- src/bgs_engine/utils/pyutils_test.cpp
+++ src/bgs_engine/utils/pyutils_test.cpp
@@ -4,12 +4,24 @@
 
 TEST(PyUtils, CanDoVectorContainmentStr) {
     std::vector<std::string> test({"one", "two", "three", "four"});
-    EXPECT_TRUE(pyutils::contains(test, "one"));
-    EXPECT_FALSE(pyutils::contains(test, "five"));
+    EXPECT_TRUE(pyutils::in("one", test));
+    EXPECT_FALSE(pyutils::in("five", test));
 }
 
 TEST(PyUtils, CanDoVectorContainmentInt) {
     std::vector<int> test({1, 2, 3, 4});
-    EXPECT_TRUE(pyutils::contains(test, 3));
-    EXPECT_FALSE(pyutils::contains(test, 8));
+    EXPECT_TRUE(pyutils::in(3, test));
+    EXPECT_FALSE(pyutils::in(8, test));
+}
+
+TEST(PyUtils, CanDoVectorContainmentSetInt) {
+    std::set<int> test({1, 2, 3, 4});
+    EXPECT_TRUE(pyutils::in(3, test));
+    EXPECT_FALSE(pyutils::in(8, test));
+}
+
+TEST(PyUtils, CanDoVectorContainmentUnorderedSetInt) {
+    std::unordered_set<int> test({1, 2, 3, 4});
+    EXPECT_TRUE(pyutils::in(3, test));
+    EXPECT_FALSE(pyutils::in(8, test));
 }
